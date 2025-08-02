b6c595fbbdf15be4a5aab682106f2c650df00eeb
Jeerapat Sripumngoen <jeerapatsepu@gmail.com>
2025-08-02 20:26:53 +0700

    first commit

---
+++ Aluar POC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aluar POC
+//
+//  Created by Jeerapat Sripumngoen on 2/8/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
